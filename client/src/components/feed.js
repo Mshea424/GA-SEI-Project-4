e5e73c062d8b6a938d771bcad8d5a9fa56986d5e
Mike Shea <mshea424@yahoo.com>
2020-06-10 16:31:31 -0400

    Set up directories for react front end

---
+++ client/src/components/feed.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export default class feed extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
