2c5351ce0920aa1146156febe641d31cb090baed
Mike Shea <mshea424@yahoo.com>
2020-06-10 20:08:11 -0400

    Added framework & Ajax request on mount for feed component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import {Route, BrowserRouter as Router, Switch} from 'react-router-dom'
+import feed from './components/feed'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={feed}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- client/src/components/feed.js
+++ client/src/components/feed.js
@@ -2,10 +2,42 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 export default class feed extends Component {
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        this.getFeed()
+        
+    }
+
+    getFeed = async () => {
+        try {
+            const res = await axios.get('/api/v1/feed/')
+            const newState = {...this.state}
+            newState.posts = res.data
+            this.setState(newState)
+            console.log(res.data)
+        } catch (error) {
+            console.log('---- COULD NOT RETRIEVE FEED DATA ----')
+            console.log(error)
+            console.log('--------------------------------------')
+        }
+    }
+
     render() {
         return (
             <div>
-                
+                <h1>Feed Page</h1>
+                {this.state.posts.map((v) =>{
+                    return (
+                        <div>
+                            <div>{v.name}</div>
+                            <img src={v.photo_url} width="400" />
+                            <div>{v.rating}</div>
+                            <div>Reviews: {v.reviews.length}</div>
+                        </div>)
+                })}
             </div>
         )
     }
