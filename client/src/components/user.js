d7301fee337c8baed752cfa84f86660ecc42e0f6
Mike Shea <mshea424@yahoo.com>
2020-06-11 14:06:54 -0400

    synced up AJAX get request for all users on User component

--- client/src/components/user.js
+++ client/src/components/user.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 export default class user extends Component {
+    state = {
+        users: []
+    }
+
+    componentDidMount() {
+        this.getUsers()
+    }
+
+    getUsers = async () => {
+        try {
+            const res = await axios.get('/api/v1/user/')
+            let newState = {...this.state}
+            newState.users = res.data
+            this.setState(newState)
+            console.log(res.data)
+        } catch (error) {
+            console.log('---- COULD NOT RETRIEVE USER DATA ----')
+            console.log(error)
+            console.log('--------------------------------------')
+        }
+    }
+
     render() {
         return (
             <div>
